1bfa9ef7d334f434c5abaeac9fcfe74acf36ea9d
Kevin Jiang <kev2018@mit.edu>
2020-03-20 18:08:54 -0400

    Randomize ChoreBot Placement
    - randomized which door has chorebot

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,15 +1,43 @@
-let doorImage1 = document.getElementById('door1');
-let doorImage2 = document.getElementById('door2');
-let doorImage3 = document.getElementById('door3');
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
-let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
-let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+const doorImage1 = document.getElementById('door1');
+const doorImage2 = document.getElementById('door2');
+const doorImage3 = document.getElementById('door3');
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+let openDoor1;
+let openDoor2;
+let openDoor3;
+const numClosedDoors = 3;
+const randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoors);
+    switch(choreDoor) {
+        case 0:
+            openDoor1 = botDoorPath;
+            openDoor2 = beachDoorPath;
+            openDoor3 = spaceDoorPath;
+            break;
+        case 1:
+            openDoor1 = beachDoorPath;
+            openDoor2 = botDoorPath;
+            openDoor3 = spaceDoorPath;
+            break;
+        case 2:
+            openDoor1 = beachDoorPath;
+            openDoor2 = spaceDoorPath;
+            openDoor3 = botDoorPath;
+            break;
+    }
+}
+
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
 }; 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 }; 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 }; 
+
+randomChoreDoorGenerator();
