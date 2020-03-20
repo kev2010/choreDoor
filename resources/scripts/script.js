fcca227cd216fc9299b818e180e9b10a9a391bea
Kevin Jiang <kev2018@mit.edu>
2020-03-20 19:23:38 -0400

    Add script.js specs

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,21 +1,28 @@
+//  Images for doors
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 const closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+//  Relevant elements by ID
 const doorImage1 = document.getElementById('door1');
 const doorImage2 = document.getElementById('door2');
 const doorImage3 = document.getElementById('door3');
 const startButton = document.getElementById('start');
 const currentStreakButton = document.getElementById('current-streak');
 const bestStreakButton = document.getElementById('best-streak');
+//  Doors to change images once clicked
 let openDoor1;
 let openDoor2;
 let openDoor3;
 let numClosedDoors = 3;
-let currentlyPlaying = true;
+//  Keep track of scores
 let bestStreak = 0;
 let currentStreak = 0;
+let currentlyPlaying = true;
 
+/**
+ * Generates a random door to contain ChoreBot. Assigns the rest of the doors the beach and space scenes.
+ */
 const randomChoreDoorGenerator = () => {
     const choreDoor = Math.floor(Math.random() * numClosedDoors);
     switch(choreDoor) {
@@ -37,14 +44,28 @@ const randomChoreDoorGenerator = () => {
     }
 }
 
+/**
+ * Returns whether or not the user opened Chore Bot's door
+ * @param {Element} door the door to check
+ * @returns boolean true if contains Chore Bot
+ */
 const isBot = (door) => {
     return door.src === botDoorPath;
 }
 
+/**
+ * Returns whether or not the door has already been opened
+ * @param {Element} door the door to check
+ * @returns boolean true if door has been opened
+ */
 const isClicked = (door) => {
     return !door.src === closedDoorPath;
 }
 
+/**
+ * Decides when to send game to end state
+ * @param {Element} door the door that has been clicked
+ */
 const playDoor = (door) => {
     numClosedDoors--;
     if (numClosedDoors === 0) {
